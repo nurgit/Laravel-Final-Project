a094c994b1f8ccec9efa44b890fe96442b410a22
SHUVRA ROY <61594591+ShuvraRoy@users.noreply.github.com>
2020-08-28 01:55:47 +0600

    homepage_view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,145 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
 
-        <title>Laravel</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@469&family=Roboto&family=Roboto+Condensed&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@469&display=swap" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <link rel ="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <link rel ="stylesheet" href ="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/jquery.bxslider.css">
+</head>
+<body>
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
 
-            .content {
-                text-align: center;
-            }
+  <header class="header">
+     <ul class="bxslider">
+     <li>
+     	<div class="bg-video">
+     		<video  playsinline="playsinline"
+     		autoplay="autoplay" muted="muted"
+     		loop="loop" id="herovideo">
+     			<source
+     			src="assets/production ID_4017223.mp4">
+     		</video>
+     		<div class="container">
+     			<div class="vidContent">
+     			<h1 class="text-left">WELCOME TO E-LEARNING</h1>
+     			<P class="text-left">
+     				CLICK HERE TO SIGN UP
 
-            .title {
-                font-size: 84px;
-            }
+     			</P>
+     			<a href="/user/signup" id="vidBtn">SIGN UP</a>
+     		</div>
+			</div>
+     	<ul class="social-icons list-inline">
+     	<li><a href=""><i class="fa fa-facebook"></i></a></li>
+     	<li><a href=""><i class="fa fa-twitter"></i></a></li>
+     	<li><a href=""><i class="fa fa-linkedin"></i></a></li>
+     	<li><a href=""><i class="fa fa-youtube"></i></a></li>
+     	</ul>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+     	</div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+     </li>
+     <li>
+     	<div class="bg-video">
+     		<video  playsinline="playsinline"
+     		autoplay="autoplay" muted="muted"
+     		loop="loop" id="herovideo">
+     			<source
+     			src="assets/production ID_4495519.mp4">
+     		</video>
+     		<div class="container">
+     			<div class="videContent">
+     			<h1 class="text-left">WELCOME TO E-LEARNING</h1>
+     			<P class="text-left">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+     			</P>
+     			<a href="" id="vidBtn">Click Here</a>
+     		</div>
+			</div>
+     	<ul class="social-icons list-inline">
+     	<li><a href=""><i class="fa fa-facebook"></i></a></li>
+     	<li><a href=""><i class="fa fa-twitter"></i></a></li>
+     	<li><a href=""><i class="fa fa-linkedin"></i></a></li>
+     	<li><a href=""><i class="fa fa-youtube"></i></a></li>
+     	</ul>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+
+     	</div>
+
+     </li>
+     </ul>
+      <nav class="nav">
+          <ul>
+             <li class="logo"><a href=""><img src="assets/img/logo.png"
+                                              alt="" class="white"><img src="assets/img/logo.png" class="black" alt=""></a></li>
+              <li><a href="">home</a></li>
+              <li><a href="">courses</a></li>
+              <li><a href="">test preparation</a></li>
+              <li><a href="">tutor</a></li>
+              <li><a href="">contact</a></li>
+              <li><a href="/login">Login</a></li>
+              <li><a href="/user/signup">Signup</a></li>
+
+
+
+              </ul>
+             <a class="nav-icon"
+             href="javascript:void(0);"><span></span><span></span><span></span></a>
+
+      </nav>
+   </header>
+    <div style="height: 500px;"></div>
+     <div class="full-nav">
+        <div class="logo"><a href=""><img
+         src="assets/img/logo.png" alt=""></a></div>
+
+         <nav class="nav2">
+             <ul>
+                 <li><a href=""><span>01.</span>Home</a></li>
+                 <li><a href=""><span>02.</span>Features</a></li>
+                 <li><a href=""><span>03.</span>work</a></li>
+                 <li><a href=""><span>04.</span>Team</a></li>
+                 <li><a href=""><span>06.</span>User</a></li>
+                 <li><a href=""><span>07.</span>Blog</a></li>
+                 <li><a href=""><span>08.</span>Contact</a></li>
+             </ul>
+
+
+         </nav>
+
+         <ul class="social-icons list-inline">
+             <li><a href=""><i class="fa fa-facebook"></i></a></li>
+             <li><a href=""><i class="fa fa-twitter"></i></a></li>
+             <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+             <li><a href=""><i class="fa fa-instagram"></i></a></li>
+         </ul>
+
+         <div class="corner-circle">
+             <a href="javascript:void(0);" class="nav-close"><span></span><span></span></a>
+         </div>
+
+     </div>
+
+
+
+
+
+
+
+   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.js"></script>
+   <script src="assets/js/main.js"></script>
+
+</body>
 </html>

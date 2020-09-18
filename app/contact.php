844f249c22fa303278d58e1543739c4ef8ab9521
Nur Mohammad <47762059+nurgit@users.noreply.github.com>
2020-09-19 01:19:02 +0600

    commit for pull

--- app/contact.php
+++ app/contact.php
@@ -4,12 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class contact extends Model
+class Contact extends Model
 {
-  protected $table = "contact";
-  public $timestamps = false;
-  /*const CREATED_AT = "create_time";
-  const UPDATED_AT = "update_time";*/
-  protected $primaryKey = "id";
-    //
+    protected $table = "contact";
+	public $timestamps = false;
+	/*const CREATED_AT = "create_time";
+	const UPDATED_AT = "update_time";*/
+	protected $primaryKey = "id";
 }
